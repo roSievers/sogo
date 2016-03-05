a78db22e75aadd0d106ad6c2821266e816d3e792
Rolf Sievers <rosievers@googlemail.com>
2016-03-05 18:21:34 +0100

    Forgott to add a file

---
+++ src/human_ai.rs
@@ -0,0 +1,55 @@
+
+use std::io;
+use game;
+use game::{GameState, PointState, PlayerColor};
+use ai;
+
+pub enum HumanPlayer {
+    Active
+}
+
+pub fn print_gamestate(state : &GameState) {
+    // Here I am iterating y, z, x in that strange order to get a nice output.
+    println!("  ----   ----   ----   ---- ");
+    for y in (0..4).rev() {
+        let mut line = "".to_string();
+        for z in 0..4 {
+            line = line + " |";
+            for x in 0..4 {
+                let flat_coordinate = game::flatten(x, y, z);
+                line = line + match state.points[flat_coordinate as usize] {
+                    PointState::Empty => ".",
+                    PointState::Piece(PlayerColor::White) => "X",
+                    PointState::Piece(PlayerColor::Black) => "O"
+                }
+            }
+            line = line + "|";
+        }
+        println!("{}", line);
+    }
+    println!("  ----   ----   ----   ---- ");
+}
+
+pub fn ask_for_move() -> ai::Move {
+    let mut instruction = String::new();
+
+    io::stdin().read_line(&mut instruction).expect("Failed to read line");
+
+    let s = String::from("0123456789ABCDEF");
+    let _index = s.find(instruction.chars().nth(0).unwrap());
+
+    let index = match _index {
+        None    => return ai::Move::Surrender,
+        Some(i) => i as i8
+    };
+
+    return ai::Move::Play { x : index % 4, y : index / 4};
+}
+
+impl ai::SogoAI for HumanPlayer {
+    fn reset_game(&self) { }
+    fn execute_move(&self, state : &game::GameState) -> ai::Move {
+        print_gamestate(state);
+        ask_for_move()
+    }
+}
