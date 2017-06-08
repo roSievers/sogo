96c832b9adf4b33b2246f56514164463467b49e0
Rolf Sievers <rolf.sievers@posteo.de>
2017-06-08 12:57:21 +0200

    Fix tests.

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -18,15 +18,15 @@ fn match_mc() {
 fn match_mc_tree() {
     let structure = Rc::new(game::Structure::new(&LINES));
     let mut white_player = ai::mc::MonteCarloAI::new(structure.clone(), 1000);
-    let mut black_player = ai::tree::TreeJudgementAI::new(structure.clone(), 2);
+    let mut black_player = ai::tree::TreeJudgementAI::new(structure.clone(), 2, ai::value::Simple::Subsets);
     run_match(&structure, &mut white_player, &mut black_player);
 }
 
 #[test]
 fn match_tree() {
     let structure = Rc::new(game::Structure::new(&LINES));
-    let mut white_player = ai::tree::TreeJudgementAI::new(structure.clone(), 2);
-    let mut black_player = ai::tree::TreeJudgementAI::new(structure.clone(), 2);
+    let mut white_player = ai::tree::TreeJudgementAI::new(structure.clone(), 2, ai::value::Simple::Subsets);
+    let mut black_player = ai::tree::TreeJudgementAI::new(structure.clone(), 2, ai::value::Simple::Subsets);
     let result = run_match(&structure, &mut white_player, &mut black_player);
     // As the TreeJudgementAI is deterministic, the same player wins all the time.
     if let VictoryState::Win {winner, ..} = result.victory_state {
